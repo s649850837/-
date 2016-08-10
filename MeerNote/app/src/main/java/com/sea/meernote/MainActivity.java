a50913cb8e2f421344728d77244e30630c0077b1
shl <649850837@qq.com>
2016-08-10 17:40:49 +0800

    导入volley 框架

--- MeerNote/app/src/main/java/com/sea/meernote/MainActivity.java
+++ MeerNote/app/src/main/java/com/sea/meernote/MainActivity.java
@@ -3,7 +3,12 @@ package com.sea.meernote;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.android.volley.toolbox.Volley;
+
+
 public class MainActivity extends AppCompatActivity {
+    private Volley v;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
